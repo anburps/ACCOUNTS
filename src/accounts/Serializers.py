23c29c5de21ddede08a4b7a93866cbe2f0b7f23d
anburps <anbalaganrps@gmail.com>
2025-01-24 20:20:28 +0530

    user register serializer added

---
+++ src/accounts/Serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,validators=[validate_password])
+    conform_password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        conform_password = attrs.get('conform_password')
+        if password != conform_password:
+            raise serializers.ValidationError("Password and conform password doesn't match")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('conform_password')
+        user = User.objects.create_user(**validated_data)
+        return user
