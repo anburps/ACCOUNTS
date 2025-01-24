b7a0e638723639d20b8f5b75a28abb38db57b5a0
anburps <anbalaganrps@gmail.com>
2025-01-24 20:13:33 +0530

    project setup

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django_rest_framework import from django.views.generic import CreateView
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
