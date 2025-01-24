20d145fe7f53215849c4b1d661a72533157e2748
anburps <anbalaganrps@gmail.com>
2025-01-24 20:41:02 +0530

    user register view funtion added

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-from django_rest_framework import from django.views.generic import CreateView
-# Create your views here.
-def index(request):
-    return render(request,'index.html')
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . models import *
+from accounts.Serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class UserRegisterView(APIView):
+    authentication_classes = [TokenAuthentication,jwtAuthentication]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend,SearchFilter]
+    search_fields = ['email']
+    ordering_fields = ['username','email']
+    pagination_class = PageNumberPagination
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
